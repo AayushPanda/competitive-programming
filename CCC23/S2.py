7b76dddda3c7f65bf3c1717399d4d0e026385b49
AayushPanda <aayush.vinayak@gmail.com>
2023-02-25 22:08:54 -0500

    add stuff

--- CCC23/S2.py
+++ CCC23/S2.py
@@ -3,45 +3,33 @@ Aayush Panda
 '''
 
 import sys
-#from itertools import repeat
+from itertools import repeat
+
+12345
+
+import sys
+from itertools import repeat
+
 
 def main():
-    #input = sys.stdin.readline
+    input = sys.stdin.readline
     print = sys.stdout.writelines
     println = lambda x: print(x + '\n')
-    #loop = lambda x: repeat(None, x)
+    loop = lambda x: repeat(None, x)
 
     maxn = int(1e5) + 2
 
     n = int(input())
     h = list(map(int, input().split()))
+    p = [0 for _ in range(n * 2)]
 
-    print(str("0 "))
+    print(str("0"))
 
-    for i in range(2, n+1):
-        assym_min = maxn
-        for j in range(n-i+1):
-            a  = h[j:i+j]
-            b = a[i//2:]
-            a = a[:i//2]
+    for win_size in range(2, n + 1):
 
-            m = 'm'
+        for o in range(0, n - win_size + 1):
+            offset = o + 0.5 * win_size % 2 == 0
+            border = win_size // 2 + 0.5 * win_size == 0
 
-            if i %2!=0: 
-                m = b[0]
-                b = b[1:]
-            b = b[::-1]
-            
-            assym = 0
 
-            for k in range(i//2):
-                assym += abs(a[k] - b[k])
-                if assym > assym_min:
-                    break
-            
-            if assym < assym_min:
-                assym_min = assym
-    
-        print(str(assym_min) + ' ')
-            
 main()
